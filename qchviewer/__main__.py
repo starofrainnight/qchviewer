5f6adbb90ae2b67c1b6ea1f827814c4911ee47f1
Hong-She Liang <starofrainnight@gmail.com>
2017-08-08 21:26:59 +0800

    Completed main routine
    Tested on Ubuntu 16.04 x64

--- qchviewer/__main__.py
+++ qchviewer/__main__.py
@@ -1,14 +1,63 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import os
+import sys
 import click
+import tempfile
+import os.path
+import jinja2
+
+
+def system(cmd):
+    click.echo(cmd)
+    os.system(cmd)
+
 
 @click.command()
-def main(args=None):
-    """Console script for qchviewer"""
-    click.echo("Replace this message by putting your code into "
-               "qchviewer.__main__.main")
-    click.echo("See click documentation at http://click.pocoo.org/")
+@click.argument('qchfile')
+def main(**kwargs):
+    """
+    Console script for qchviewer
+
+    QCHFILE: The qch file path
+    """
+
+    if not os.path.isfile(kwargs["qchfile"]):
+        raise click.BadParameter(
+            "%s not a valid qch file!" % kwargs["qchfile"])
+
+    template = jinja2.Template("""\
+<?xml version="1.0" encoding="utf-8" ?>
+<QHelpCollectionProject version="1.0">
+	<assistant>
+		<title>{{ title }}</title>
+		<currentFilter>Default</currentFilter>
+		<enableAddressBar visible="true">true</enableAddressBar>
+	</assistant>
+	<docFiles>
+		<register>
+			<file>{{ path }}</file>
+		</register>
+	</docFiles>
+</QHelpCollectionProject>
+
+""")
+
+    with tempfile.TemporaryDirectory() as temp_dir:
+        qhcp_path = os.path.realpath(os.path.join(temp_dir, "main.qhcp"))
+        qhcp_content = template.render(
+            title=os.path.basename(kwargs["qchfile"]),
+            path=os.path.realpath(kwargs["qchfile"]))
+
+        with open(qhcp_path, "wb") as qhcp_file:
+            qhcp_file.write(qhcp_content.encode('utf-8'))
+
+        qhc_path = os.path.realpath(os.path.join(temp_dir, "main.qhc"))
+
+        system("qcollectiongenerator \"%s\" -o \"%s\"" % (qhcp_path, qhc_path))
+
+        system("assistant -collectionFile \"%s\"" % qhc_path)
 
 
 if __name__ == "__main__":
